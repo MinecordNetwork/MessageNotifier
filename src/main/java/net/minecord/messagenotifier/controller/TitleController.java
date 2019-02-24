3bc2e5b8962707e6dc95e231c3bfdd976e430d96
Rick Strafy <rixafy@gmail.com>
2019-02-24 23:44:17 +0100

    Implemented sending titles with event

--- src/main/java/net/minecord/messagenotifier/controller/TitleController.java
+++ src/main/java/net/minecord/messagenotifier/controller/TitleController.java
@@ -2,6 +2,7 @@ package net.minecord.messagenotifier.controller;
 
 import net.minecord.messagenotifier.MessageNotifier;
 import net.minecord.messagenotifier.entity.WelcomeTitle;
+import net.minecord.messagenotifier.events.WelcomeTitleSendEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,12 +11,14 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class TitleController {
 
     private MessageNotifier messageNotifier;
     private List<WelcomeTitle> titles = new ArrayList<>();
     private boolean sendAll;
+    private Random random = new Random();
 
     public TitleController(MessageNotifier messageNotifier) {
         this.messageNotifier = messageNotifier;
@@ -35,9 +38,42 @@ public class TitleController {
     public void onJoin(Player player) {
         new BukkitRunnable() {
             public void run() {
+                if (!sendAll) {
+                    WelcomeTitle randomTitle = titles.get(random.nextInt(titles.size()));
+                    WelcomeTitleSendEvent welcomeTitleSendEvent = new WelcomeTitleSendEvent(player, randomTitle);
 
+                    if (welcomeTitleSendEvent.isCancelled()) {
+                        cancel();
+                    }
+
+                    sendTitle(player, randomTitle);
+
+                } else {
+                    int delayTicks = 0;
+                    for (WelcomeTitle welcomeTitle : titles) {
+                        sendTitle(player, welcomeTitle, delayTicks);
+                        delayTicks += welcomeTitle.getFadeIn() + welcomeTitle.getStay();
+                    }
+                }
             }
         }.runTaskAsynchronously(messageNotifier);
+    }
+
+    private void sendTitle(Player player, WelcomeTitle welcomeTitle, int delay) {
+        if (delay != 0) {
+            new BukkitRunnable() {
+                public void run() {
+                    sendTitle(player, welcomeTitle);
+                }
+            }.runTaskLaterAsynchronously(messageNotifier, delay);
+
+        } else {
+            sendTitle(player, welcomeTitle);
+        }
+    }
+
+    private void sendTitle(Player player, WelcomeTitle welcomeTitle) {
+        player.sendTitle(ChatColor.translateAlternateColorCodes('&', welcomeTitle.getTitle()), ChatColor.translateAlternateColorCodes('&', welcomeTitle.getSubTitle()), welcomeTitle.getFadeIn(), welcomeTitle.getStay(), welcomeTitle.getFadeOut());
     }
 
     public void onDisable() {
